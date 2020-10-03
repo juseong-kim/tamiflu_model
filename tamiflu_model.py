5be80b9b93054d0387169581d46120efb4a26ca1
Max Golovsky <max@excursionist.com>
2020-10-03 11:36:57 -0400

    Worked out issues in multi-dosing equations

--- tamiflu_model.py
+++ tamiflu_model.py
@@ -8,21 +8,13 @@ ke = 0.136  # OC elimintation rate
 
 Go = 75  # initial dose of 75mg
 OPo = Go * ka
-t = np.arange(0, 36, .1)  # 24 hours, iterate by tenth of hour
 
 tdose = 12  # time between doses in hours
-dt = .1 # dose time
-second_dose = False
+num_doses = 4
+dt = .1  # dose time
 
 
-def dose(t):
-    if t % tdose <= dt:
-        return Go / dt
-    else:
-        return 0
-
-
-def OP_metabolism(Y, t):
+def OP_metabolism_multidose(Y, t):
     G = Y[0]
     OP = Y[1]
     OC = Y[2]
@@ -31,21 +23,10 @@ def OP_metabolism(Y, t):
     dOPdt = ka * G - kf * OP
     dOCdt = kf * OP - ke * OC
 
-    return [dGdt, dOPdt, dOCdt, Y[3]]
-
-def OP_metabolism_multidose(Y, t):
-    G = Y[0] + dose(t)
-    OP = Y[1]
-    OC = Y[2]
-
-    dGdt = -1 * ka * G
-    dOPdt = ka * G - kf * OP
-    dOCdt = kf * OP - ke * OC
-
-    return [dGdt, dOPdt, dOCdt, Y[3]]
+    return [dGdt, dOPdt, dOCdt]
 
 
-def plot(G, OP, OC):
+def plot(G, OP, OC, t):
     fig = plt.figure(num=1, clear=True)
     ax = fig.add_subplot(1, 1, 1)
     # Plot using red circles
@@ -65,11 +46,36 @@ def plot(G, OP, OC):
     fig.savefig('Oseltamivir_Metabolism.png')
 
 
-Yo = [75, 0, 0, False]  # initial conditions of G, dose, is 75mg and OP and OC are both 0
+def dosing(tdose, num_doses):
+    dose = 1  # Indexing from 1 feels dirty, but its the way that I have to store time data
+    G = []  # Outer variable storage for overall concentrations
+    OP = []
+    OC = []
+    T = []
+    Yo = [75, 0, 0]  # initial conditions of G, dose, is 75mg and OP and OC are both 0
+    t = np.arange(0, tdose, .01)  # 12 hours, iterate by the minute
+    while dose <= num_doses:
+        # This function runs a new ODE for each dose, saving the final condition of one dose
+        # and feeding it in as the initial condition of the next dose
+        # lower case letters represent concentration after a given dose, upper case are total concentrations
 
-out = odeint(OP_metabolism, Yo, t)
-G = out[:, 0]
-OP = out[:, 1]
-OC = out[:, 2]
+        out = odeint(OP_metabolism_multidose, Yo, t)
+        g = out[:, 0]
+        op = out[:, 1]
+        oc = out[:, 2]
+        G.extend(g)
+        OP.extend(op)
+        OC.extend(oc)
 
-plot(G, OP, OC)
+        T.extend(t + dose * tdose)
+        dose += 1
+        added_dose = g[len(g) - 1] + 75
+        Yo = [added_dose, op[len(op) - 1], oc[len(oc) - 1]]
+
+    print(G)
+    print(OP)
+    print(OC)
+    plot(G, OP, OC, T)
+
+
+dosing(12, 7)  # Function call for the dosing equation, frequency of dosing, number of doses
